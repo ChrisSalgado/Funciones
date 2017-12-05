ade79c75f1d744540adaae746156be0141223cf5
Christopher Alexander Salgado <cristopher.salgado1997@gmail.com>
2017-12-04 21:06:45 -0600

    html nav listo

--- index.php
+++ index.php
@@ -4,10 +4,32 @@
     <meta charset="UTF-8">
     <title>Inicio</title>
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <link rel="stylesheet" href="css/style.css">
+    <script src="js/jquery.js"></script>
     <link rel="stylesheet" href="css/estilo.css">
+    <link rel="stylesheet" href="fonts.css">
 </head>
 <body>
     
+    <header>
+        <div class="menu_bar">
+            <a href="#" class="bt-menu"><span class="icon-menu"></span>Menu</a>
+        </div>
+        
+        <nav>
+            <ul>
+                <li><a href="#"><span class="icon-home"></span>Item #1</a></li>
+                <li><a href="#"><span class="icon-rocket"></span>Item #2</a></li>
+                <li><a href="#"><span class="icon-earth"></span>Item #3</a></li>
+                <li><a href="#"><span class="icon-envelop"></span>Item #3</a></li>
+                <li><a href="#"><span class="icon-user"></span>Item #3</a></li>
+            </ul>
+        </nav>
+        
+    </header>
+    
+    
+    
+    
+    <script src="js/index.js"></script>
 </body>
 </html>
