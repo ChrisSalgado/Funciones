17360341fe449ef6eaa80e4c8e4be661e234be6b
Christopher Alexander Salgado <cristopher.salgado1997@gmail.com>
2017-12-04 22:59:09 -0600

    vale veinte la vida con este menu
    no funcionó el responsive.

--- index.php
+++ index.php
@@ -4,24 +4,23 @@
     <meta charset="UTF-8">
     <title>Inicio</title>
     <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-    <script src="js/jquery.js"></script>
     <link rel="stylesheet" href="css/estilo.css">
     <link rel="stylesheet" href="fonts.css">
 </head>
 <body>
     
     <header>
-        <div class="menu_bar">
-            <a href="#" class="bt-menu"><span class="icon-menu"></span>Menu</a>
+        <div class="menu_bar" id="menu">
+            <a href="#" class="bt-menu" ><span class="icon-menu"></span>Funcionalidades</a>
         </div>
         
-        <nav>
+        <nav id="nave">
             <ul>
                 <li><a href="#"><span class="icon-home"></span>Inicio</a></li>
                 <li><a href="#"><span class="icon-rocket"></span>Proyectos</a></li>
                 <li><a href="#"><span class="icon-earth"></span>Nosotros</a></li>
                 <li><a href="#"><span class="icon-envelop"></span>Contacto</a></li>
-                <li clas="submenu">
+                <li class="submenu">
                     <a href="#"><span class="icon-user"></span>Usuario<span class="caret icon-circle-down"></span></a>
                     <ul class="children">
                         <li><a href="#">Registrarse <span class="icon-circle-left"></span></a></li>
@@ -36,6 +35,7 @@
     
     
     
-    <script src="js/index.js"></script>
+   <script src="js/jquery.js"></script>
+    <script src="js/index.js"></script> 
 </body>
 </html>
