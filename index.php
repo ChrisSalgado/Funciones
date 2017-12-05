a17e2f785218a8d0527f3b200389d16d8cbac3d1
Chape <andres.chapeton0206@gmail.com>
2017-12-04 22:58:43 -0600

    Iniciar sesión
    Formulario de Iniciar sesión (diseño)

--- index.php
+++ index.php
@@ -3,8 +3,21 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <link rel="stylesheet" href="css/estilo.css">
 </head>
-<body>
+<body> 
+   
+    <div class="contenedor">
+        <div class="form">
+           <h2 class="form_titulo">Iniciar Sesión</h2> 
+           <input type="text" class="form_input">
+           <label for="" class="form_label">Usuario</label>
+           <input type="password" class="form_input">
+           <label for="" class="form_label">Contraseña</label>
+           <input type="submit" class="form_submit">
+        </div>
+    </div>
     
+    <script src="js/form.js"></script>
 </body>
 </html>
