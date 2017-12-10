64d887737e584b1a317d7a30dad56fab8bacecc4
Christopher Alexander Salgado <cristopher.salgado1997@gmail.com>
2017-12-10 00:51:03 -0600

    hay va

--- asset/insertar.php
+++ asset/insertar.php
@@ -1,12 +1,11 @@
 <?php
-error_reporting (0);
-include ('conexion.php');
 
 $mysqli = new mysqli('localhost', 'root', '', 'funciones');
 
-
-        if($_SERVER['REQUEST_METHOD'] == 'POST'){
-            //passwords are equals
+error_reporting (0);
+include ('conexion.php');
+        
+       //passwords are equals
             if($_POST['pass'] == $_POST['Repetirpass']){
                 
                 $nombre = $mysqli->real_escape_string($_POST["nombre"]);
@@ -15,59 +14,55 @@ $mysqli = new mysqli('localhost', 'root', '', 'funciones');
                 $Fecha = $mysqli->real_escape_string($_POST["Fecha"]);
                 $pass = md5($_POST['pass']);
                 $correo = $mysqli->real_escape_string($_POST["correo"]);
-                
+        
                 //Consulta para insertar
                 $insertar = "INSERT INTO usuario (Nombre, Apellido, DUI, Fecha, pass, correo) values ('".$nombre."', '".$apellido."', '".$DUI."', '".$Fecha."', '".$pass."', '".$correo."')";
                 
                 //verificar si ya esta insertado
-                $verificar_correo = mysqli_query($conexion, "SELECT * FROM usuario WHERE correo = '$correo'");
-                if(mysqli_num_rows($verificar_usuario) > 0 ){
+                $verificar_correo = mysqli_query($conexion, "SELECT correo FROM usuario WHERE correo = '$correo'");
+                if(mysqli_num_rows($verificar_correo) > 0 ){
                     echo '<script>
-                            alert("El registro fue agregado con exito");
+                            alert("Este correo ya existe");
                             window.history.go(-1);
                           </script>   
                     ';
                     exit;
                 }
                 
-                $verificar_DUI = mysqli_query($conexion, "SELECT * FROM usuario WHERE DUI = '$DUI'");
-                if(mysqli_num_rows($verificar_usuario) > 0 ){
+                $verificar_DUI = mysqli_query($conexion, "SELECT DUI FROM usuario WHERE DUI = '$DUI'");
+                if(mysqli_num_rows($verificar_DUI) > 0 ){
                     echo '<script>
-                            alert("El registro fue agregado con exito");
+                            alert("El numero de DUI ya existe");
                             window.history.go(-1);
                           </script>   
                     ';
                     exit;
                 }
                 
-                
-                //
+
+                //Ejecutar consulta
                 $resultado = mysqli_query($conexion, $insertar);
-                
-                if($mysqli->query($insertar) == true){
-                    echo '<script>
-                            alert("El registro fue agregado con exito");
-                            window.history.go(-1);
-                          </script>   
-                    ';
+
+                if(!$resultado){
+                     echo '<script>
+                                    alert("El registro no fue agregado");
+                                    window.history.go(-1);
+                                  </script>   
+                            ';
                 }else{
                     echo '<script>
-                            alert("El registro no fue agregado");
-                            window.history.go(-1);
-                          </script>
-                           
-                    ';
-                }
-                
+                                    alert("El registro fue agregado con exito");
+                                    window.history.go(-1);
+                                  </script>
+                            ';
+                }    
             }else{
-               echo '<script>
+                echo '<script>
                             alert("Las contra tiene que ser iguales");
                             window.history.go(-1);
                           </script>
                     ';
-            }
-        }
-
+                }
            
         //mysqli_free_result($query);
         mysqli_close($conexion);
