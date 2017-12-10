3975f4f2b26db4aec9fce150840a4af99129a2fa
Chape <andres.chapeton0206@gmail.com>
2017-12-09 20:07:38 -0600

    :v

--- js/validaciones.js
+++ js/validaciones.js
@@ -1,9 +1,9 @@
-if (document.form_registro.nombre.value.length ==0){
+if (Register.form_registro.Nombre.value.length ==0){
     alert("Campo de nombre vacío");
-    document.form_registro.nombre.focus();
+    document.form_registro.Nombre.focus();
     return false;
-} else if(document.form_registro.nombre.value.match(/[a-z]/)){
+} else if(Register.form_registro.Nombre.value.match(/[a-z]/)){
     alert("Solo se permiten letras");
-    document.form_registro.nombre.focus();
+    document.form_registro.Nombre.focus();
     return false;
 }
