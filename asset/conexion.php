4f70c855b1f7eee6468a501b752802d21221b7c4
Christopher Alexander Salgado <cristopher.salgado1997@gmail.com>
2017-12-09 20:11:52 -0600

    commit

--- asset/conexion.php
+++ asset/conexion.php
@@ -1,13 +1,7 @@
 <?php
-    $conexion = mysqli_connect('localhost', 'root', '');
+    $conexion = mysqli_connect('localhost', 'root', '', 'funciones');
     
     if (!$conexion) {
         die("Database connection failed: " . mysqli_error());
-    }else
-
-    $my_select = mysqli_select_db($conexion, 'funciones');
-
-    if(!$my_select){
-        die("Database selection failed: " . mysqli_error());
-    }
+    }   
 ?>
